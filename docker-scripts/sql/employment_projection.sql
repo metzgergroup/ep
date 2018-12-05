36885a490cd4f5e26101850d6bb29d077dbfb318
Brian Findlay <bfindlay@fastmail.com>
2018-12-05 12:34:58 -0500

    Convert numbers from decimals to integers

--- docker-scripts/sql/employment_projection.sql
+++ docker-scripts/sql/employment_projection.sql
@@ -11,6 +11,10 @@ CREATE UNLOGGED TABLE employment_projection (
 \set filepath `echo ${DATA_DIR}`/employment_projection.csv
 COPY employment_projection (occupation_code, industry_code, employment_2016, employment_2026) FROM :'filepath' WITH CSV;
 
+ALTER TABLE employment_projection
+    ALTER COLUMN employment_2016 SET DATA TYPE integer USING employment_2016 * 1000,
+    ALTER COLUMN employment_2026 SET DATA TYPE integer USING employment_2026 * 1000;
+
 ALTER TABLE employment_projection SET LOGGED;
 
 \echo
