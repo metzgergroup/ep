a9c067a1f555ce7a122b04ffb9d9d9f20d994b55
Brian Findlay <bfindlay@fastmail.com>
2020-01-16 06:55:26 -0500

    Use generalized column names instead of specific years

--- docker-scripts/sql/employment_projection.sql
+++ docker-scripts/sql/employment_projection.sql
@@ -1,15 +1,15 @@
 \echo employment_projection
 
 CREATE UNLOGGED TABLE employment_projection (
-    occupation_code text REFERENCES occupation,
-    industry_code   text REFERENCES industry,
-    employment_2018 decimal,
-    employment_2028 decimal,
+    occupation_code   text REFERENCES occupation,
+    industry_code     text REFERENCES industry,
+    employment_recent decimal,
+    employment_future decimal,
     PRIMARY KEY (occupation_code, industry_code)
 );
 
 \set filepath `echo ${DATA_DIR}`/employment_projection.csv
-COPY employment_projection (occupation_code, industry_code, employment_2018, employment_2028) FROM :'filepath' WITH CSV;
+COPY employment_projection (occupation_code, industry_code, employment_recent, employment_future) FROM :'filepath' WITH CSV;
 
 ALTER TABLE employment_projection SET LOGGED;
 
